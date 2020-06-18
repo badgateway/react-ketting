f60684994cd14ddeb5c2c8876c68aa03e6bbb740
Evert Pot <me@evertpot.com>
2020-06-18 00:54:55 -0400

    DocsDocs

--- src/hooks/use-read-resource.ts
+++ src/hooks/use-read-resource.ts
@@ -7,6 +7,35 @@ type UseReadResourceResult<T> = {
   resourceState: ResourceState<T>
 }
 
+/**
+ * Hook for fetching and subscribing to state changes on Ketting resources.
+ *
+ * The hook returns an object with three properties:
+ * 1. loading (boolean)
+ * 2. error (Error|null)
+ * 3. resourceState - A ketting State object.
+ *
+ * The hook will automatically update its internal state if Ketting received
+ * 'update' events.
+ *
+ * Example usage:
+ *
+ * <pre>
+ *  function MyComponent(resource: Resource<Article>) {
+ *
+ *     const {loading, error, resourceState} = useReadResource(resource);
+ *     if (loading) return <p>Loading...</p>;
+ *     if (error) return <div class="error">Error: ${err.message}</div>
+ *
+ *     return <article>
+ *       <h1>${resourceState.title}</h1>
+ *       <p>${resoourceState.body}</p>
+ *     </article>;
+ *
+ *  }
+ * </pre>
+ *
+ */
 export function useReadResource<T>(resource: Resource<T>): UseReadResourceResult<T> {
 
   const [loading, setLoading] = useState(true);
