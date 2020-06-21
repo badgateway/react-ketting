4cb3136560b9e1f51cd7249f49e6729037fe0ea6
Evert Pot <me@evertpot.com>
2020-06-21 00:58:24 -0400

    Docs

--- src/hooks/use-resource.ts
+++ src/hooks/use-resource.ts
@@ -26,6 +26,62 @@ type UseResourceOptions<T> = {
   initialState: T | ResourceState<T>,
 }
 
+/**
+ * The useResource hook allows you to GET and PUT the state of
+ * a resource.
+ *
+ * Example call:
+ *
+ * <pre>
+ *   const {
+ *     loading,
+ *     error,
+ *     resourceState,
+ *     setResourceState,
+ *     submit
+ *  } = useResource(resource);
+ * </pre>
+ * 
+ * Returned properties:
+ *
+ * * loading - will be true as long as the result is still being fetched from
+ *             the server.
+ * * error - Will be null or an error object.
+ * * resourceState - A state object. The `.data` property of this object will
+ *                   contain the parsed JSON from the server.
+ * * setResourceState - Update the local cache of the resource.
+ * * submit - Send a PUT request to the server.
+ *
+ * If you don't need the full resourceState, you can also use the `data` and
+ * `setData` properties instead of `resourceState` or `useResourceState`.
+ *
+ * It's also possible to use useResource for making new resources. In this case
+ * a POST request will be done instead on a 'collection' resource.
+ *
+ * If the response to the POST request is 201 Created and has a Location header,
+ * subsequent calls to `submit()` turn into `PUT` requests on the new resource,
+ * fully managing the lifecycle of creation, and subsequent updates to the
+ * resource.
+ *
+ * Example call:
+ *
+ * <pre>
+ *   const {
+ *     loading,
+ *     error,
+ *     data,
+ *     setData,
+ *     submit
+ *  } = useResource({
+ *    resource: resource,
+ *    mode: 'POST',
+ *    initialState: { foo: bar, title: 'New article!' }
+ *  });
+ * </pre>
+ *
+ * To do POST requests you must specifiy initialState with the state the user starts
+ * off with.
+ */
 export function useResource<T>(resource: Resource<T>): UseResourceResponse<T>;
 export function useResource<T>(options: UseResourceOptions<T>): UseResourceResponse<T>;
 export function useResource<T>(arg1: Resource<T>|UseResourceOptions<T>): UseResourceResponse<T> {
